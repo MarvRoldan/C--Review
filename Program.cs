e5e59b3e1a5f292df2686f8cda1aab17b6b1292a
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-08 09:56:30 -0600

    Added operations

--- Program.cs
+++ Program.cs
@@ -12,6 +12,30 @@ namespace C__Review
             */
             int number = 2147483647;
             long longnumber = 2313213213214598317;
+            // float decimalnumber
+            // double longdecimalnumber
+            // decimal verylongdecimalnumber
+
+            char myChar = 'H'; // One character. Single qoutes for char.
+            string myString = "Hello!"; // One character or string of characters.
+
+            bool myTruthy = true || false; 
+
+            /*
+            * Operations
+            */
+
+            // Arithmetic
+            int n = 2;
+            int i = 3;
+
+            int addResult = i + n;
+            int subtractResult = i -n;
+            int multiplicationResult = i * n;
+            int divisionResult = i /n;
+            int modulusResult = i % n;
+            int incrementResult = n++;
+            int decrementResult = n--;
 
         }
     }
