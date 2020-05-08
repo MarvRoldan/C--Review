5440b4f92cb57965f2b14e8f432a6aa65d4f8b14
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-08 11:09:05 -0600

    Added while, and for loops

--- Program.cs
+++ Program.cs
@@ -63,8 +63,8 @@ namespace C__Review
             /*
             * Math Method
             */
-            Console.WriteLine(Math.Max(2,3));
-            Console.WriteLine(Math.Min(2,3));
+            Console.WriteLine(Math.Max(2, 3));
+            Console.WriteLine(Math.Min(2, 3));
             Console.WriteLine(Math.Sqrt(81));
             Console.WriteLine(Math.Abs(-5));
             Console.WriteLine(Math.Round(3.14));
@@ -78,13 +78,86 @@ namespace C__Review
             Console.WriteLine(myStringTwo.ToLower());
 
             string[] myStringArray = myStringTwo.Split(" ");
-            foreach (string element in myStringArray) {
+            foreach (string element in myStringArray)
+            {
                 Console.WriteLine(element);
             }
 
             string Greeter = $"\"{myString} {myName}\"";
             Console.WriteLine(Greeter);
 
+            /*
+            * If / Else / Else if
+            */
+            string season = "Spring";
+
+            if (season == "Fall")
+            {
+                Console.WriteLine("It's fall, leaves everywhere!");
+            }
+            else if (season == "Winter")
+            {
+                Console.WriteLine("It's winter, show everywhere!");
+            }
+            else if (season == "Spring")
+            {
+                Console.WriteLine("It's spring, rain everywhere!");
+            }
+            else
+            {
+                Console.WriteLine("It's summer, bugs everywhere!");
+            }
+
+            switch (season)
+            {
+                case "Fall":
+                    Console.WriteLine("It's fall, leaves everywhere!");
+                    break;
+                case "Winter":
+                    Console.WriteLine("It's winter, snow everywhere!");
+                    break;
+                case "Summer":
+                    Console.WriteLine("It's summer, bugs everywhere!");
+                    break;
+                default:
+                    Console.WriteLine("No such season.");
+                    break;
+            }
+
+            /*
+            * While / Do While Loop
+            */
+            int counter = 0;
+
+            while (counter < 3)
+            {
+                counter++;
+                Console.WriteLine(counter);
+            }
+
+            counter = 0;
+
+            do // This will run at least once.
+            {
+                counter++;
+                Console.WriteLine(counter);
+            } while (counter < 3);
+
+            /*
+            * For / Foreach Loops
+            */
+
+            int[] myArray = { 1, 2, 3, 4 };
+
+            for (int pos = 0; pos < myArray.Length; pos += 2)
+            {
+                myArray[pos] = myArray[pos] * 2;
+            }
+
+            foreach (int element in myArray)
+            {
+                Console.WriteLine(element);
+            }
         }
     }
 }
