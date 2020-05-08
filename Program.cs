84951dd56b282922c2086d622c3353f48cd84bd6
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-08 09:46:22 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C__Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*
+            * Datatypes
+            */
+            int number = 2147483647;
+            long longnumber = 2313213213214598317;
+
+        }
+    }
+}
